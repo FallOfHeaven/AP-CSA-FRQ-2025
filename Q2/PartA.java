7982e1c5b66ce85b89b46276400ff4bae3dc3ac8
FallOfHeaven <vincentc268@nycstudents.net>
2025-05-13 11:37:44 -0400

    Update PartA.java

--- Q2/PartA.java
+++ Q2/PartA.java
@@ -1,1 +1,26 @@
+public class SignedText{
+    private String first;
+    private String last;
 
+    public SignedText(String first, String last){
+        this.first = first;
+        this.last = last;
+    }
+
+    public String getSignature(){
+        if(first.length() == 0){
+            return last;
+        }
+        return first.substring(0,1) + "-"+ last;
+    }
+
+    public String addSignature(String add){
+        if(add.indexOf(getSignature()) == -1){
+            return add + getSignature();
+        }
+        if(add.indexOf(getSignature()) == 0){
+            return add.substring(getSignature().length()) + getSignature();
+        }
+        return add;
+    }
+}
