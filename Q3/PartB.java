f4d4acfa83144344cc602d615c4d7c2f77116e13
FallOfHeaven <vincentc268@nycstudents.net>
2025-05-13 11:54:32 -0400

    Update PartB.java

--- Q3/PartB.java
+++ Q3/PartB.java
@@ -1,1 +1,16 @@
-
+public ArrayList<Match> buildMatches(){
+  ArrayList<Match> m = new ArrayList<Match>();
+  int size = competitorList.size().size();
+  if(size % 2 == 0){
+    for(int i = 0; i < size; i++){
+      Match e = new Match(competitorList.get(i), competitorList.get(size - i -1 ));
+      m.add(e);
+    }
+    return m;
+  }
+  for(int i = 1; i < size; i++){
+    Match o = new Match(competitorList.get(i), competitorList.get(size - i));
+    m.add(o);
+  }
+  return m;
+}
