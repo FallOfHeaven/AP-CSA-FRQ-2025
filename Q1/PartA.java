915d933b70e2f931828be475f5a07993048d788e
FallOfHeaven <vincentc268@nycstudents.net>
2025-05-13 16:06:17 -0400

    Update PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,1 +1,9 @@
-
+public int walkDogs(int hour){
+    int totalDogs = numAvailableDogs(hour);
+    if(totalDogs - maxDogs >= 0){
+        updateDogs(hour, maxDogs);
+        return maxDogs;
+    }
+    updateDogs(hour, totalDogs);
+    return totalDogs;
+}
